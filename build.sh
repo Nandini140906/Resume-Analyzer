f9f87ae49fa5ce2a6fe6a3c9d7b2642198ef4e77
Nandini <nandinidas001@outlook.com>
2026-03-23 02:45:48 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,3 @@
-#!/bin/bash
 set -e
 pip install --upgrade pip setuptools wheel
-pip install --only-binary=:all: pandas==1.5.3 numpy==1.24.3
-pip install -r requirements.txt --no-deps --ignore-installed pandas numpy
 pip install -r requirements.txt
