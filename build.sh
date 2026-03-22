36b1332e77578fd82d022e678991eb0d03120c63
Nandini Das <nandinii14954@gmail.com>
2026-03-23 01:20:24 +0530

    git commit -m "Update build.sh and requirements.txt and remove streamlit and update pandas version" git commit -m "Update runtime.txt"

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
-pip install --upgrade pip
-pip install --only-binary=:all: pandas numpy
+#!/bin/bash
+set -e
+pip install --upgrade pip setuptools wheel
+pip install --only-binary=:all: pandas==1.5.3 numpy==1.24.3
+pip install -r requirements.txt --no-deps --ignore-installed pandas numpy
 pip install -r requirements.txt
